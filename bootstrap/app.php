52161917fd8eaf81efdfa33e201b319a55bb92ab
Iván López Ordorica <webmasterbrak@gmail.com>
2022-10-23 08:48:22 +0200

    Add container in app file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,12 @@
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use DI\Container;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
