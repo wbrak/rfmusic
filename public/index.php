28a39ea742554eb1d85f8ea3471a76f451445dc2
Iván López Ordorica <webmasterbrak@gmail.com>
2022-10-23 12:50:33 +0200

    Change index in folder public

--- public/index.php
+++ public/index.php
@@ -1,12 +1,4 @@
 <?php
 
-/**
- * Start Session
- */
-session_start();
-
-/**
- * Autoload global dependencies and allow for auto-loading local dependencies via use
- */
-require_once __DIR__ . '/../bootstrap/app.php';
+(require __DIR__ . '/../bootstrap/app.php')->run();
 
