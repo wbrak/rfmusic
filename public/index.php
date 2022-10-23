04c85e74fc0f56f5f1fc63037ad58df55c1ab994
Iván López Ordorica <webmasterbrak@gmail.com>
2022-10-23 08:41:05 +0200

    Add autoload bootstrap app file in index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
 /**
  * Start Session
  */
@@ -12,13 +8,5 @@ session_start();
 /**
  * Autoload global dependencies and allow for auto-loading local dependencies via use
  */
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap/app.php';
 
-$app = AppFactory::create();
-
-$app->get( '/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello World!');
-		return $response;
-	});
-
-$app->run();
